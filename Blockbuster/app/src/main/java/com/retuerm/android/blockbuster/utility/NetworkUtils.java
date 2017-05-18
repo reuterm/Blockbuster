7258231df5e8c382aeaf16f515220d905ed8aca1
Max Reuter <max.a.reuter@gmail.com>
2017-05-18 23:19:09 +0300

    add utility functions for trailer

--- Blockbuster/app/src/main/java/com/retuerm/android/blockbuster/utility/NetworkUtils.java
+++ Blockbuster/app/src/main/java/com/retuerm/android/blockbuster/utility/NetworkUtils.java
@@ -16,6 +16,8 @@ public class NetworkUtils {
 
     public static String BASE_URL = "https://api.themoviedb.org";
 
+    public static String BASE_TRAILER_URL = "http://www.youtube.com/watch";
+
     public static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
@@ -24,7 +26,10 @@ public class NetworkUtils {
     public interface TheMovieDBService {
         @GET("3/movie/{sortingMode}")
         Call<MovieList> listMoviesSortedBy(@Path("sortingMode") String sortingMode,
-                                                      @Query("api_key") String apiKey);
+                                           @Query("api_key") String apiKey);
+        @GET("3/movie/{movieId}/videos")
+        Call<MovieTrailerList> getMovieTrailer(@Path("movieId") int movieId,
+                                               @Query("api_key") String apiKey);
     }
 
     public static String buildMoviePosterURL (String posterPath) {
@@ -35,6 +40,16 @@ public class NetworkUtils {
                 .addPathSegments("t/p/w500")
                 // Drop leading "/" because OkHTTP will try to encode it
                 .addEncodedPathSegment(posterPath.substring(1))
+                .build();
+        return url.toString();
+    }
+
+    public static String buildYoutubeURL (String key) {
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("https")
+                .host("www.youtube.com")
+                .addPathSegment("watch")
+                .addQueryParameter("v", key)
                 .build();
         return url.toString();
     }
