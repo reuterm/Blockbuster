b18f7bb43b436a963ad90bcc00747e0a918a7c9e
Max Reuter <max.a.reuter@gmail.com>
2017-05-18 23:17:14 +0300

    extend MovieItem

--- Blockbuster/app/src/main/java/com/retuerm/android/blockbuster/utility/MovieItem.java
+++ Blockbuster/app/src/main/java/com/retuerm/android/blockbuster/utility/MovieItem.java
@@ -8,35 +8,43 @@ import android.os.Parcelable;
  */
 
 // Helper class that represents a single movie
-public class MovieItem  implements Parcelable {
+public class MovieItem implements Parcelable {
+    private int id;
     private String title;
     private String release_date;
     private String overview;
     private double vote_average;
     private String poster_path;
+    private boolean favourite;
 
-    public MovieItem(String title, String releaseDate, String plotSynopsis, double averageRating,
+    public MovieItem(int id, String title, String releaseDate, String plotSynopsis, double averageRating,
                      String imageURL) {
+        this.id = id;
         this.title = title;
         this.release_date = releaseDate;
         this.overview = plotSynopsis;
         this.vote_average = averageRating;
         poster_path = imageURL;
+        this.favourite = false;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public String getRelease_date() {
+    public String getReleaseDate() {
         return release_date;
     }
 
-    public String getOverview() {
+    public String getPlotSynopsis() {
         return overview;
     }
 
-    public double getVote_average() {
+    public double getVoteAverage() {
         return vote_average;
     }
 
@@ -44,9 +52,21 @@ public class MovieItem  implements Parcelable {
         return NetworkUtils.buildMoviePosterURL(poster_path);
     }
 
+    public String getPosterPath() {
+        return poster_path;
+    }
+
+    public boolean isFavourite() {
+        return favourite;
+    }
+
+    public void setFavourite(boolean status) {
+        favourite = status;
+    }
+
     public static final Parcelable.Creator<MovieItem> CREATOR = new Parcelable.Creator<MovieItem>() {
         public MovieItem createFromParcel(Parcel in) {
-            return new MovieItem(in.readString(), in.readString(), in.readString(), in.readDouble(),
+            return new MovieItem(in.readInt(), in.readString(), in.readString(), in.readString(), in.readDouble(),
                     in.readString());
         }
 
@@ -62,6 +82,7 @@ public class MovieItem  implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
         dest.writeString(title);
         dest.writeString(release_date);
         dest.writeString(overview);
